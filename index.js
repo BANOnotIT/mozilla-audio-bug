08e9c462625f8bc537277d12d83fa148c28a35c4
BANO.notIT <bano.notit@yandex.ru>
2020-08-21 22:19:11 +0300

    add variant without WA api

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const media = new Audio('./test.wav')
-
-// media.playbackRate = 2
+// const media = new Audio('./test.wav')
 
 media.addEventListener('canplaythrough', () => {
     if (media.readyState === media.HAVE_ENOUGH_DATA) {
@@ -15,7 +13,23 @@ media.addEventListener('canplaythrough', () => {
                 media.playbackRate = 2
             })
 
+            media.controls = true
             btn.disabled = true
         }
     }
 })
+
+media1.addEventListener('canplaythrough', () => {
+    if (media1.readyState === media.HAVE_ENOUGH_DATA) {
+        btn1.disabled = false
+        btn1.onclick = () => {
+            media1.play().then(() => {
+                media1.playbackRate = 2
+            })
+
+            media1.controls = true
+            btn1.disabled = true
+        }
+    }
+})
+
